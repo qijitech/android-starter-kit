6f8e6f640b83f0573fe6fac3264262f97c2ff72f
杨玉刚 <smartydroid@gmail.com>
2015-10-02 10:41:49 +0800

    update loading more

--- library/src/main/java/com/smartydroid/android/starter/kit/widget/LoadMoreView.java
+++ library/src/main/java/com/smartydroid/android/starter/kit/widget/LoadMoreView.java
@@ -6,39 +6,49 @@ package com.smartydroid.android.starter.kit.widget;
 
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import com.smartydroid.android.starter.kit.R;
 import com.smartydroid.android.starter.kit.utilities.ViewUtils;
 
 public class LoadMoreView implements ILoadMoreView {
 
   private View mContainer;
-  private View mContainerContent;
+  private TextView mTextView;
   private ProgressBar mProgress;
 
   @Override
   public void init(FootViewAdder footViewHolder, View.OnClickListener onClickLoadMoreListener) {
     mContainer = footViewHolder.addFootView(R.layout.include_loadmore_view);
-    mProgress = ViewUtils.getView(mContainer, android.R.id.secondaryProgress);
-    mContainerContent = ViewUtils.getView(mContainer, R.id.container_loadmore_content);
+
+    if (onClickLoadMoreListener != null) {
+      mContainer.setOnClickListener(onClickLoadMoreListener);
+    }
+
+    mProgress = ViewUtils.getView(mContainer, android.R.id.progress);
+    mTextView = ViewUtils.getView(mContainer, android.R.id.text1);
   }
 
   @Override public void showLoading() {
     ViewUtils.setGone(mProgress, false);
-    ViewUtils.setGone(mContainerContent, true);
+    ViewUtils.setGone(mTextView, false);
+    mTextView.setText(R.string.starter_loadingmore);
   }
 
   @Override public void showNoMore() {
     ViewUtils.setGone(mProgress, true);
-    ViewUtils.setGone(mContainerContent, false);
+    ViewUtils.setGone(mTextView, false);
+    mTextView.setText(R.string.starter_loadingmore_nomore);
   }
 
   @Override public void showNormal() {
     ViewUtils.setGone(mProgress, true);
-    ViewUtils.setGone(mContainerContent, false);
+    ViewUtils.setGone(mTextView, false);
+    mTextView.setText(R.string.starter_loadingmore_more);
   }
 
   @Override public void showFailure(Throwable t) {
     ViewUtils.setGone(mProgress, true);
-    ViewUtils.setGone(mContainerContent, false);
+    ViewUtils.setGone(mTextView, false);
+    mTextView.setText(R.string.starter_loadingmore_more);
   }
 }
