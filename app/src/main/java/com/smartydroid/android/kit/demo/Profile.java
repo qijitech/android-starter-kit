b1902318288acad8111a3800f069445101989f10
杨玉刚 <smartydroid@gmail.com>
2015-11-03 18:07:42 +0800

    update

--- app/src/main/java/com/smartydroid/android/kit/demo/Profile.java
+++ app/src/main/java/com/smartydroid/android/kit/demo/Profile.java
@@ -6,6 +6,6 @@ package com.smartydroid.android.kit.demo;
 
 public interface Profile {
 
-  String API_ENDPOINT = "http://ibet.isnacker.com";
+  String API_ENDPOINT = "http://licaishi.wodetiku.com";
   String API_ACCEPT = "application/vnd.financerapp.v1+json";
 }
