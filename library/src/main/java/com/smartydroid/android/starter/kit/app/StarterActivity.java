3b3029e43420c87a35c01ac5ca06518648ebf1d2
杨玉刚 <smartydroid@gmail.com>
2015-11-25 22:35:55 +0800

    add single pane activity

--- library/src/main/java/com/smartydroid/android/starter/kit/app/StarterActivity.java
+++ library/src/main/java/com/smartydroid/android/starter/kit/app/StarterActivity.java
@@ -4,6 +4,8 @@
  */
 package com.smartydroid.android.starter.kit.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import butterknife.ButterKnife;
@@ -101,4 +103,46 @@ public abstract class StarterActivity extends AppCompatActivity {
   public boolean isImmActive() {
     return starterCommon != null && starterCommon.isImmActive();
   }
+
+  /**
+   * Converts an intent into a {@link android.os.Bundle} suitable for use as fragment arguments.
+   */
+  public static Bundle intentToFragmentArguments(Intent intent) {
+    Bundle arguments = new Bundle();
+    if (intent == null) {
+      return arguments;
+    }
+
+    final Uri data = intent.getData();
+    if (data != null) {
+      arguments.putParcelable("_uri", data);
+    }
+
+    final Bundle extras = intent.getExtras();
+    if (extras != null) {
+      arguments.putAll(intent.getExtras());
+    }
+
+    return arguments;
+  }
+
+  /**
+   * Converts a fragment arguments bundle into an intent.
+   */
+  public static Intent fragmentArgumentsToIntent(Bundle arguments) {
+    Intent intent = new Intent();
+    if (arguments == null) {
+      return intent;
+    }
+
+    final Uri data = arguments.getParcelable("_uri");
+    if (data != null) {
+      intent.setData(data);
+    }
+
+    intent.putExtras(arguments);
+    intent.removeExtra("_uri");
+    return intent;
+  }
+
 }
