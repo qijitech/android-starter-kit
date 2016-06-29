fa65c1d9bc58e07953aa9b56a28714823f396e35
杨玉刚 <smartydroid@gmail.com>
2016-06-30 00:26:44 +0800

    fix login

--- rxapp/src/main/java/starter/kit/rx/app/feature/auth/LoginActivity.java
+++ rxapp/src/main/java/starter/kit/rx/app/feature/auth/LoginActivity.java
@@ -2,6 +2,8 @@ package starter.kit.rx.app.feature.auth;
 
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import butterknife.Bind;
@@ -11,12 +13,17 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import nucleus.factory.RequiresPresenter;
 import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import starter.kit.retrofit.ErrorResponse;
 import starter.kit.retrofit.RetrofitException;
 import starter.kit.rx.app.R;
 import starter.kit.rx.app.RxStarterActivity;
 import starter.kit.rx.app.model.entity.User;
+import support.ui.app.SupportApp;
 import work.wanghao.simplehud.SimpleHUD;
+
+import static rx.android.schedulers.AndroidSchedulers.mainThread;
 
 /**
  * Created by YuGang Yang on 06 29, 2016.
@@ -39,16 +46,100 @@ public class LoginActivity extends RxStarterActivity<AuthPresenter> {
   }
 
   private void setupObservable() {
-    Observable.combineLatest(RxTextView.textChanges(mUsernameEdit).map(charSequence ->
-          charSequence != null && charSequence.length() >= 6),
-        RxTextView.textChanges(mPasswordEdit).map(charSequence ->
-            charSequence != null && charSequence.length() >= 6),
-        (username, password) -> username && password)
-        .subscribe(RxView.enabled(mLoginBtn));
+
+    Observable<Boolean> usernameChangeObservable = RxTextView.textChanges(mUsernameEdit)
+        .doOnNext(charSequence -> hideUsernameError())
+        .debounce(400, TimeUnit.MILLISECONDS)
+        .filter(charSequence -> !TextUtils.isEmpty(charSequence))
+        .map(charSequence -> validateUsername(charSequence.toString()));
+
+    Observable<Boolean> passwordChangeObservable = RxTextView.textChanges(mPasswordEdit)
+        .doOnNext(charSequence -> hidePasswordError())
+        .debounce(400, TimeUnit.MILLISECONDS)
+        .filter(charSequence -> !TextUtils.isEmpty(charSequence))
+        .map(charSequence -> validatePassword(charSequence.toString()));
+
+    // Checks for validity of the username input field
+    Subscription usernameSubscription = usernameChangeObservable
+        .observeOn(mainThread()) // UI Thread
+        .subscribe(validFields -> {
+          if (validFields) {
+            hideUsernameError();
+          } else {
+            showUsernameError();
+          }
+        });
+    add(usernameSubscription);
+
+    // Checks for validity of the password input field
+    Subscription passwordSubscription = passwordChangeObservable
+        .observeOn(mainThread()) // UI Thread
+        .subscribe(validFields -> {
+          if (validFields) {
+            hidePasswordError();
+          } else {
+            showPasswordError();
+          }
+        });
+    add(passwordSubscription);
+
+    // Checks for validity of all input fields
+    Subscription btnSubscription =
+        Observable.combineLatest(usernameChangeObservable, passwordChangeObservable,
+            (isUsernameValid, isPasswordValid) -> isUsernameValid && isPasswordValid)
+            .observeOn(AndroidSchedulers.mainThread()) // UI Thread
+            .subscribe(RxView.enabled(mLoginBtn));
+    add(btnSubscription);
+
+    //Observable.combineLatest(RxTextView.textChanges(mUsernameEdit).map(charSequence ->
+    //      charSequence != null && charSequence.length() >= 6),
+    //    RxTextView.textChanges(mPasswordEdit).map(charSequence ->
+    //        charSequence != null && charSequence.length() >= 6),
+    //    (username, password) -> username && password)
+    //    .subscribe(RxView.enabled(mLoginBtn));
 
     RxView.clicks(mLoginBtn)
         .debounce(300, TimeUnit.MILLISECONDS)
         .subscribe(aVoid -> doLogin());
+  }
+
+  private boolean validateUsername(String username) {
+    return username != null && username.trim().length() >= 6;
+  }
+
+  private boolean validatePassword(String password) {
+    return password != null && password.trim().length() >= 6;
+  }
+
+  private void enableError(TextInputLayout textInputLayout) {
+    if (textInputLayout.getChildCount() == 2)
+      textInputLayout.getChildAt(1).setVisibility(View.VISIBLE);
+  }
+
+  private void disableError(TextInputLayout textInputLayout) {
+    if (textInputLayout.getChildCount() == 2)
+      textInputLayout.getChildAt(1).setVisibility(View.GONE);
+  }
+
+  private void showUsernameError(){
+    enableError(mUsernameContainer);
+    //mUsernameContainer.setErrorEnabled(true);
+    mUsernameContainer.setError(SupportApp.appResources().getString(R.string.login_username_error));
+  }
+
+  private void hideUsernameError(){
+    disableError(mUsernameContainer);
+    mUsernameContainer.setError(null);
+  }
+
+  private void showPasswordError(){
+    enableError(mPasswordContainer);
+    mPasswordContainer.setError(SupportApp.appResources().getString(R.string.login_username_error));
+  }
+
+  private void hidePasswordError(){
+    disableError(mPasswordContainer);
+    mPasswordContainer.setError(null);
   }
 
   private void setupViews() {
--- rxlibrary/src/main/java/starter/kit/rx/app/RxStarterActivity.java
+++ rxlibrary/src/main/java/starter/kit/rx/app/RxStarterActivity.java
@@ -3,8 +3,20 @@ package starter.kit.rx.app;
 import butterknife.ButterKnife;
 import nucleus.presenter.Presenter;
 import nucleus.view.NucleusAppCompatActivity;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
 
 public class RxStarterActivity<P extends Presenter> extends NucleusAppCompatActivity<P> {
+
+  private final CompositeSubscription subscriptions = new CompositeSubscription();
+
+  public void add(Subscription subscription) {
+    subscriptions.add(subscription);
+  }
+
+  public void remove(Subscription subscription) {
+    subscriptions.remove(subscription);
+  }
 
   @Override public void setContentView(int layoutResID) {
     super.setContentView(layoutResID);
@@ -14,5 +26,6 @@ public class RxStarterActivity<P extends Presenter> extends NucleusAppCompatActi
   @Override protected void onDestroy() {
     super.onDestroy();
     ButterKnife.unbind(this);
+    subscriptions.unsubscribe();
   }
 }
