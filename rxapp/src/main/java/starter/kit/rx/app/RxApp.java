11508c1f9d097e5599989f7ea72a74c3ea3a10a1
杨玉刚 <smartydroid@gmail.com>
2016-07-20 23:28:09 +0800

    add drawer in rxapp

--- rxapp/src/main/java/starter/kit/rx/app/RxApp.java
+++ rxapp/src/main/java/starter/kit/rx/app/RxApp.java
@@ -1,6 +1,15 @@
 package starter.kit.rx.app;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerUIUtils;
 import starter.kit.retrofit.Network;
 
 public class RxApp extends RxStarterApp {
@@ -14,5 +23,37 @@ public class RxApp extends RxStarterApp {
         .build();
 
     Fresco.initialize(appContext());
+
+    //initialize and create the image loader logic
+    DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+      @Override public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+        Glide.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
+      }
+
+      @Override public void cancel(ImageView imageView) {
+        Glide.clear(imageView);
+      }
+
+      @Override public Drawable placeholder(Context ctx, String tag) {
+        //define different placeholders for different imageView targets
+        //default tags are accessible via the DrawerImageLoader.Tags
+        //custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
+        if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
+          return DrawerUIUtils.getPlaceHolder(ctx);
+        } else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
+          return new IconicsDrawable(ctx).iconText(" ")
+              .backgroundColorRes(com.mikepenz.materialdrawer.R.color.primary)
+              .sizeDp(56);
+        } else if ("customUrlItem".equals(tag)) {
+          return new IconicsDrawable(ctx).iconText(" ")
+              .backgroundColorRes(R.color.md_red_500)
+              .sizeDp(56);
+        }
+
+        //we use the default one for
+        //DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
+        return super.placeholder(ctx, tag);
+      }
+    });
   }
 }
--- rxapp/src/main/java/starter/kit/rx/app/feature/main/MainActivity.java
+++ rxapp/src/main/java/starter/kit/rx/app/feature/main/MainActivity.java
@@ -1,24 +1,76 @@
 package starter.kit.rx.app.feature.main;
 
 import android.os.Bundle;
-import android.widget.FrameLayout;
-import butterknife.Bind;
+import android.support.v7.widget.Toolbar;
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import starter.kit.rx.app.R;
 import starter.kit.rx.app.RxStarterActivity;
-import starter.kit.rx.app.feature.feed.FeedFragment;
 
 public class MainActivity extends RxStarterActivity {
 
-  @Bind(R.id.fragmentContainer) FrameLayout frameLayout;
+  //save our header or result
+  private AccountHeader headerResult = null;
+  private Drawer result = null;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    if (savedInstanceState == null) {
-      getSupportFragmentManager().beginTransaction()
-          .replace(R.id.fragmentContainer, new FeedFragment())
-          .commit();
+    // Handle Toolbar
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    //set the back arrow in the toolbar
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setTitle(R.string.app_name);
+
+    // Create a few sample profile
+    // NOTE you have to define the loader logic too. See the CustomApplication for more details
+    final IProfile profile = new ProfileDrawerItem().withName("Smartydroid")
+        .withEmail("smartydroid@gmail.com")
+        .withIcon("https://avatars2.githubusercontent.com/u/13810934?v=3&s=460");
+
+    // Create the AccountHeader
+    headerResult = new AccountHeaderBuilder().withActivity(this)
+        .withHeaderBackground(R.drawable.header)
+        .addProfiles(profile)
+        .withSavedInstance(savedInstanceState)
+        .build();
+
+    //Create the drawer
+    result = new DrawerBuilder().withActivity(this)
+        .withToolbar(toolbar)
+        .withHasStableIds(true)
+        .withAccountHeader(headerResult) //set the AccountHeader we created earlier for the header
+        .addStickyDrawerItems(
+            new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog).withIdentifier(10),
+            new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github)
+        )
+        .withSavedInstance(savedInstanceState)
+        .withShowDrawerOnFirstLaunch(true)
+        .build();
+  }
+
+  @Override protected void onSaveInstanceState(Bundle outState) {
+    //add the values which need to be saved from the drawer to the bundle
+    outState = result.saveInstanceState(outState);
+    //add the values which need to be saved from the accountHeader to the bundle
+    outState = headerResult.saveInstanceState(outState);
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override public void onBackPressed() {
+    //handle the back press :D close the drawer first and if the drawer is closed close the activity
+    if (result != null && result.isDrawerOpen()) {
+      result.closeDrawer();
+    } else {
+      super.onBackPressed();
     }
   }
 }
