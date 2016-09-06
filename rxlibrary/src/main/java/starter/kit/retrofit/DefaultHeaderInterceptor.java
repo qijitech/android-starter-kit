bac38b37fc9411369f0ee5c505521bffce9f9ac5
杨玉刚 <smartydroid@gmail.com>
2016-09-06 22:13:36 +0800

    refactor network

--- rxlibrary/src/main/java/starter/kit/retrofit/DefaultHeaderInterceptor.java
+++ rxlibrary/src/main/java/starter/kit/retrofit/DefaultHeaderInterceptor.java
@@ -1,53 +1,31 @@
 package starter.kit.retrofit;
 
-import android.text.TextUtils;
 import java.io.IOException;
 import okhttp3.Headers;
 import okhttp3.Request;
 import okhttp3.Response;
+import starter.kit.account.AccountManager;
 import starter.kit.util.Strings;
-import support.ui.app.SupportApp;
-import support.ui.utilities.AppInfo;
 
 /**
  * @author <a href="mailto:smartydroid.com@gmail.com">Smartydroid</a>
  */
 public class DefaultHeaderInterceptor implements HeaderInterceptor {
 
-  String mToken;
-  /**
-   * example: application/vnd.xxx.v1+json
-   */
-  String mApiVersionAccept;
-
-  public DefaultHeaderInterceptor(String token, String apiAccept) {
-    mToken = token;
-    mApiVersionAccept = apiAccept;
+  private Headers.Builder builder;
+  public DefaultHeaderInterceptor(Headers.Builder builder) {
+    this.builder = builder;
   }
 
   @Override public Response intercept(Chain chain) throws IOException {
     Request originalRequest = chain.request();
-    Headers.Builder builder = new Headers.Builder();
-    final AppInfo appInfo = SupportApp.appInfo();
-
-    builder.add("Content-Encoding", "gzip")
-        .add("X-Client-Build", String.valueOf(appInfo.versionCode))
-        .add("X-Client-Version", appInfo.version)
-        .add("X-Client", appInfo.deviceId)
-        .add("X-Language-Code", appInfo.languageCode)
-        .add("X-Client-Type", "android");
-
-    final String channel = appInfo.channel;
-    if (! TextUtils.isEmpty(channel)) {
-      builder.add("X-Client-Channel", channel);
+    if (builder == null) {
+      builder = new Headers.Builder();
     }
 
-    if (!Strings.isBlank(mToken)) {
-      builder.add("Authorization", "Bearer " + mToken);
-    }
-
-    if (!Strings.isBlank(mApiVersionAccept)) {
-      builder.add("Accept", mApiVersionAccept);
+    final String token = AccountManager.INSTANCE.token();
+    if (!Strings.isBlank(token)) {
+      builder.set("Authorization", "Bearer " + token);
     }
 
     Request compressedRequest = originalRequest
--- rxlibrary/src/main/java/starter/kit/retrofit/Network.java
+++ rxlibrary/src/main/java/starter/kit/retrofit/Network.java
@@ -1,9 +1,16 @@
 package starter.kit.retrofit;
 
+import android.text.TextUtils;
+import com.orhanobut.logger.Logger;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import starter.kit.util.Preconditions;
+import starter.kit.util.Strings;
+import support.ui.app.SupportApp;
+import support.ui.utilities.AppInfo;
 
 public final class Network {
 
@@ -44,7 +51,9 @@ public final class Network {
   public static class Builder {
     private String baseUrl;
     private String accept;
+    private Headers.Builder headerBuilder;
     private OkHttpClient mClient;
+    private boolean networkDebug;
 
     public Network build() {
       Preconditions.checkNotNull(baseUrl, "Base URL required.");
@@ -59,7 +68,24 @@ public final class Network {
 
     private void ensureSaneDefaults() {
       if (mClient == null) {
-        mClient = OkHttpManager.get().okHttpClient(accept);
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        if (networkDebug) {
+          HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor((message) -> Logger.d(message));
+          loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+          builder.addNetworkInterceptor(loggingInterceptor);
+        }
+
+        if (headerBuilder == null) {
+          headerBuilder = defaultHeader();
+        }
+        if (!Strings.isBlank(accept)) {
+          headerBuilder.add("Accept", accept);
+        }
+
+        DefaultHeaderInterceptor headerInterceptor = new DefaultHeaderInterceptor(headerBuilder);
+
+        builder.addInterceptor(headerInterceptor);
+        mClient = builder.build();
       }
     }
 
@@ -74,10 +100,40 @@ public final class Network {
       return this;
     }
 
+    public Builder networkDebug(boolean networkDebug) {
+      this.networkDebug = networkDebug;
+      return this;
+    }
+
     public Builder accept(String accept) {
       Preconditions.checkNotNull(accept, "accept == null");
       this.accept = accept;
       return this;
+    }
+
+    public Builder addHeader(String name, String value) {
+      if (headerBuilder == null) {
+        headerBuilder = defaultHeader();
+      }
+      headerBuilder.set(name, value);
+      return this;
+    }
+
+    private Headers.Builder defaultHeader() {
+      final AppInfo appInfo = SupportApp.appInfo();
+      Headers.Builder builder = new Headers.Builder();
+      builder.add("Content-Encoding", "gzip")
+          .add("X-Client-Build", String.valueOf(appInfo.versionCode))
+          .add("X-Client-Version", appInfo.version)
+          .add("X-Client", appInfo.deviceId)
+          .add("X-Language-Code", appInfo.languageCode)
+          .add("X-Client-Type", "android");
+
+      final String channel = appInfo.channel;
+      if (! TextUtils.isEmpty(channel)) {
+        builder.add("X-Client-Channel", channel);
+      }
+      return builder;
     }
   }
 }
