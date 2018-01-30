c8ad8656d587c0e03bab726e9913a60670a7a0e0
杨玉刚 <smartydroid@gmail.com>
2018-01-30 19:32:33 +0800

    replace Foreground views

---
+++ rxlibrary/src/main/java/support/ui/widget/ForegroundImageView.java
@@ -0,0 +1,23 @@
+/**
+ * Created by YuGang Yang on April 08, 2015.
+ * Copyright 2007-2015 Laputapp.com. All rights reserved.
+ */
+package support.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class ForegroundImageView extends com.commit451.foregroundviews.ForegroundImageView {
+
+  public ForegroundImageView(Context context) {
+    super(context);
+  }
+
+  public ForegroundImageView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public ForegroundImageView(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+  }
+}
